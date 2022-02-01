c27703d487164230b324ec17fb10771eca362d38
Thomas Mekonnen <mekonnen_t@ukw.de>
2022-02-01 13:22:25 +0100

    Solve Search Insert Position

---
+++ src/main/java/com/tomhydra/a2sv/leetcode/arrays/_35_search_insert_position/Solution.java
@@ -0,0 +1,19 @@
+package com.tomhydra.a2sv.leetcode.arrays._35_search_insert_position;
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1, mid = 0;
+
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+}
